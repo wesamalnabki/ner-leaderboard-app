452cb1a630e86f8caa76186855f5db931d317ff9
wesam-alnabki-fao <wesam.alnabki@gmail.com>
2025-04-15 08:57:01 +0200

    Add new user

--- src/add_user.py
+++ src/add_user.py
@@ -6,6 +6,12 @@ new_password = "leaderboard_-_p@@ssword"  # This is the password you want to has
 new_name = "Wesam Al Nabki"
 new_email = "wesam.alnabki@gmail.com"
 
+# User details
+new_username = "nlp4bia-bsc"
+new_password = "leaderboard_-_p@@ssword"  # This is the password you want to hash
+new_name = "nlp4bia-bsc"
+new_email = ""
+
 # Initialize the Hasher
 hashed_passwords = stauth.Hasher.hash(new_password)
 # hashed_passwords = hasher.generate()
